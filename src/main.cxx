48f59dcaf923ced95919b5ba7e2763f784b48099
æsir <accelworld108@gmail.com>
2025-01-16 17:51:11 +0100

    add character stats and skills

--- src/main.cxx
+++ src/main.cxx
@@ -1,8 +1,9 @@
 #include <SFML/Graphics.hpp>
 
 int main() {
-	sf::RenderWindow window(sf::VideoMode({960, 544}), "TRPG-Demo");
+	sf::RenderWindow window(sf::VideoMode({960, 544}), "TRPG-demo");
 	window.setVerticalSyncEnabled(true);
+
 	while (window.isOpen()) {
 		while (const std::optional event = window.pollEvent()) {
 			if (event->is<sf::Event::Closed>()) {
